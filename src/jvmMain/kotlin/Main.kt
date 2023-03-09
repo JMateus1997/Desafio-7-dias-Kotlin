fb08cba4149284a7a9b760468048d9ef676dc11a
JMateus1997 <joaomateus_22@msn.com>
2023-03-09 15:04:31 -0300

    Criação do dia 3. Implementação do estilo do layout

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,18 +1,19 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
@@ -20,10 +21,17 @@ import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.loadSvgPainter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.awaitAll
 import java.io.ByteArrayOutputStream
 import java.net.HttpURLConnection
 import java.net.URL
@@ -31,32 +39,71 @@ import javax.imageio.ImageIO
 import javax.swing.Painter
 
 
-
-
 @Composable
 @Preview
 fun App() {
 
     val image = "https://upload.wikimedia.org/wikipedia/pt/a/a7/Toy_Story_1995.jpg"
-    val image2 = "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/26E7C634EB29C137475AE05AC983080BAE7E2E3741C49987EEA2539A40A9262C/scale?width=1200&aspectRatio=1.78&format=jpeg"
-    val image3 = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fdublagem%2Fimages%2Fa%2Fa6%2FToy_Story_poster.png%2Frevision%2Flatest%3Fcb%3D20220909172215%26path-prefix%3Dpt-br&imgrefurl=https%3A%2F%2Fdublagem.fandom.com%2Fwiki%2FToy_Story&tbnid=WkkaqxSmaxM-VM&vet=12ahUKEwiGj-vwgc39AhVpHbkGHb8JDLQQMygmegUIARC7Ag..i&docid=JbQBJKXNufk5pM&w=1000&h=1500&q=toy%20story&ved=2ahUKEwiGj-vwgc39AhVpHbkGHb8JDLQQMygmegUIARC7Ag"
+    val padding = 10.dp
 
     MaterialTheme {
-        MaterialTheme{
-            Column {
-                Text("Toy Story")
-                Image(
-                    bitmap = loadNetworkImage(image),
-                    contentDescription = "Toy Story",
-                )
-                Text("Nota: 10 - Ano: 1995")
+        MaterialTheme(
+            colors = darkColors()
+        ) {
+            Box(
+
+                modifier = Modifier.background(color = Color.DarkGray).wrapContentSize().padding(16.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        bitmap = loadNetworkImage(image),
+                        contentDescription = "Toy Story",
+                    )
+                    Row(
+                        modifier = Modifier.padding(10.dp).requiredSize(height = 16.dp, width = 256.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                    ) {
+                        Box() {
+                            Row {
+                                Icon(
+                                    Icons.Rounded.Star,
+                                    contentDescription = null,
+                                    tint = Color.Yellow
+                                )
+                                Text(
+                                    color = Color.White,
+                                    text = "10",
+                                    style = TextStyle(
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 16.sp,
+                                    )
+                                )
+                            }
+                        }
+                        Text(
+                            color = Color.White,
+                            text = "Ano: 1995"
+                        )
+                    }
+                    Text(
+                        modifier = Modifier.padding(padding),
+                        text = "Toy Story",
+                        style = TextStyle(
+                            color = Color.White,
+                            fontFamily = FontFamily.Monospace,
+                            fontSize = 16.sp,
+                        )
+                    )
+                }
             }
         }
     }
 
 }
 
-fun loadNetworkImage(link: String): ImageBitmap{
+fun loadNetworkImage(link: String): ImageBitmap {
     val url = URL(link)
     val connection = url.openConnection() as HttpURLConnection
     connection.connect()
