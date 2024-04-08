094486bb52872b0383af90a44f2ed374aaf698fb
JMateus1997 <joaomateus_22@msn.com>
2024-04-08 18:18:15 -0300

    Adição de arquivos

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -2,106 +2,135 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
-import androidx.compose.ui.modifier.modifierLocalOf
-import androidx.compose.ui.res.loadImageBitmap
-import androidx.compose.ui.res.loadSvgPainter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.awaitAll
 import java.io.ByteArrayOutputStream
 import java.net.HttpURLConnection
 import java.net.URL
 import javax.imageio.ImageIO
-import javax.swing.Painter
+import Movie
+import androidx.compose.foundation.lazy.items
 
 
 @Composable
 @Preview
 fun App() {
 
-    val image = "https://upload.wikimedia.org/wikipedia/pt/a/a7/Toy_Story_1995.jpg"
+    val url_1 = "https://upload.wikimedia.org/wikipedia/pt/a/a7/Toy_Story_1995.jpg"
+    val url_2 = "https://pt.wikipedia.org/wiki/Ficheiro:Movie_poster_toy_story_2.jpg"
+    val url_3 = "https://a-static.mlcdn.com.br/800x560/poster-cartaz-toy-story-3-c-pop-arte-poster/poparteskins2/15938539965/2439854928f001ae7c84aac7c40c9e83.jpeg"
+    val url_4 = "https://pt.wikipedia.org/wiki/Ficheiro:Toy_Story_4_poster.jpg"
     val padding = 10.dp
 
     MaterialTheme {
         MaterialTheme(
             colors = darkColors()
         ) {
-            Box(
-
-                modifier = Modifier.background(color = Color.DarkGray).wrapContentSize().padding(16.dp)
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
+            Surface {
+                Box(
+                    modifier = Modifier.background(color = Color.DarkGray).wrapContentSize().padding(16.dp),
                 ) {
-                    Image(
-                        bitmap = loadNetworkImage(image),
-                        contentDescription = "Toy Story",
+                    val movies = listOf<Movie>(
+                        Movie(
+                            titulo = "Toy Story",
+                            imagem = "https://upload.wikimedia.org/wikipedia/pt/a/a7/Toy_Story_1995.jpg",
+                            nota = 10.0,
+                            ano = 1995
+                        ),Movie(
+                            titulo = "Toy Story2",
+                            imagem = "https://pt.wikipedia.org/wiki/Ficheiro:Movie_poster_toy_story_2.jpg",
+                            nota = 10.0,
+                            ano = 1999
+                        ),/*Movie(
+                            titulo = "Toy Story3",
+                            imagem = url_3,
+                            nota = 10.0,
+                            ano = 2010
+                        ),Movie(
+                            titulo = "Toy Story4",
+                            imagem = url_4,
+                            nota = 10.0,
+                            ano = 2019
+                        ),*/
                     )
-                    Row(
-                        modifier = Modifier.padding(10.dp).requiredSize(height = 16.dp, width = 256.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                    ) {
-                        Box() {
-                            Row {
-                                Icon(
-                                    Icons.Rounded.Star,
-                                    contentDescription = null,
-                                    tint = Color.Yellow
-                                )
-                                Text(
-                                    color = Color.White,
-                                    text = "10",
-                                    style = TextStyle(
-                                        fontWeight = FontWeight.Bold,
-                                        fontSize = 16.sp,
-                                    )
-                                )
-                            }
-                        }
-                        Text(
-                            color = Color.White,
-                            text = "Ano: 1995"
-                        )
+                    LazyColumn(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ){
+                        items(items = movies) { movie ->
+                            MovieItem(
+                                movie = movie
+                            )
                     }
-                    Text(
-                        modifier = Modifier.padding(padding),
-                        text = "Toy Story",
-                        style = TextStyle(
-                            color = Color.White,
-                            fontFamily = FontFamily.Monospace,
-                            fontSize = 16.sp,
-                        )
-                    )
+                    }
                 }
             }
         }
     }
 
 }
+
+@Composable
+fun MovieItem(movie: Movie){
+
+    println(movie.imagem)
+    Column {
+        Image(
+            bitmap = loadNetworkImage(movie.imagem),
+            contentDescription = "Toy Story",
+        )
+        Row(
+            modifier = Modifier.padding(10.dp).requiredSize(height = 16.dp, width = 256.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Box() {
+                Row {
+                    Icon(
+                        Icons.Rounded.Star,
+                        contentDescription = null,
+                        tint = Color.Yellow
+                    )
+                    Text(
+                        color = Color.White,
+                        text = movie.nota.toString(),
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp,
+                        )
+                    )
+                }
+            }
+            Text(
+                color = Color.White,
+                text = movie.ano.toString(),
+            )
+        }
+        Text(
+            modifier = Modifier.padding(10.dp),
+            text = movie.titulo,
+            style = TextStyle(
+                color = Color.White,
+                fontFamily = FontFamily.Monospace,
+                fontSize = 16.sp,
+            )
+        )
+    }
+}
+
 
 fun loadNetworkImage(link: String): ImageBitmap {
     val url = URL(link)
@@ -117,25 +146,11 @@ fun loadNetworkImage(link: String): ImageBitmap {
 
     return org.jetbrains.skia.Image.makeFromEncoded(byteArray).toComposeImageBitmap()
 }
-
-/*fun loadImageBitmap(url: URL): ImageBitmap{
-    val inputStream = url.openStream()
-    val bufferedImage = inputStream.buffered()
-    val stream = bufferedImage.readBytes()
-
-    return org.jetbrains.skia.Image.makeFromEncoded(stream).toComposeImageBitmap()
-}*/
-
-/*fun loadImageBitmap(url: String): ImageBitmap =
-    URL(url).openStream().buffered().use(::loadImageBitmap)
-    //URL(url).openStream().buffered().use(::loadImageBitmap)*/
-
-
-
-
-
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
 }
+
+
+
--- src/jvmMain/kotlin/Movie.kt
+++ src/jvmMain/kotlin/Movie.kt
@@ -1,2 +1,6 @@
-class Movie {
-}
+class Movie (
+    val titulo: String,
+    val imagem: String,
+    val nota: Double,
+    val ano: Int
+)
